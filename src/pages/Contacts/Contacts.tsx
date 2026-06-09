799b4c53adf72a508300339d9af5ddb163396461
ihaasbut <ihaasbut@gmail.com>
2026-06-09 20:26:34 +0300

    Поправила контакты

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import TitleBlockGrey from "../../components/TitleBlockPrimary/TitleBlockPrimary";
 import YandexMap from "../../components/YandexMap/YandexMap";
+import { useLanguage } from "../../hooks/use-language";
+import type { TitleBlockPropsI } from "../../types/titleBlock.types";
 import styles from "./Contacts.module.css";
 
 function Contacts() {
+    const [pageData, setPageData] = useState<TitleBlockPropsI | null>(null);
+    const { language } = useLanguage();
+
+    useEffect(() => {
+        (async () => {
+            const pageModule = await import(
+                `./mockData/contacts-page.mockData.${language}.ts`
+            );
+
+            setPageData(pageModule.headerPage);
+        })();
+    }, [language]);
+
+    if (!pageData) {
+        return null;
+    }
+
     return (
         <div className={styles["contacts"]}>
-            <TitleBlockGrey title={"Контакты"} />
+            <TitleBlockGrey title={pageData.title} />
 
             <YandexMap />
         </div>
---
+++ src/pages/Contacts/mockData/contacts-page.mockData.ru.ts
@@ -0,0 +1,5 @@
+import type { TitleBlockPropsI } from "../../../types/titleBlock.types";
+
+export const headerPage: TitleBlockPropsI = {
+    title: "Контакты",
+};
